f8f8de8679a921bdbdca3b0c3a4cb718eda37988
leog <leog91@gmail.com>
2026-06-17 18:26:11 +0100

    app icon updated

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -28,6 +28,16 @@ export const Route = createRootRoute({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'icon',
+        type: 'image/svg+xml',
+        href: '/app-icon.svg',
+      },
+      {
+        rel: 'alternate icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
     ],
   }),
   notFoundComponent: NotFoundPage,
